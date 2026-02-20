d72a3e1453a7655dfcc04e63c053118d57dea326
pipias860-droid <pipias860@gmail.com>
2026-02-20 21:38:45 +0400

    commit

---
+++ lvl 6/cw.py
@@ -0,0 +1,12 @@
+
+dog_name = input("what is ur dogs name?:")
+
+print(type(dog_name))
+
+birthday = input("what year where you born?:")
+
+print(birthday)
+
+age = input("how old are you?:")
+
+print(age)
