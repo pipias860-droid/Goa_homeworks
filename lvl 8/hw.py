c834d43bbb28232cc8b523cc12ff0c6dc125ec2a
pipias860-droid <pipias860@gmail.com>
2026-02-28 13:20:19 +0400

    commit

---
+++ lvl 8/hw.py
@@ -0,0 +1,44 @@
+
+
+str="sandro"
+str2="nikora"
+str3="nikoloz"
+str4="giotgi"
+str5="luka"
+
+int=1
+int=2
+int=3
+int=4
+int=5
+
+float=1.05
+float2=2.05
+float3=3.05
+float4=4.05
+float5=5.05
+
+bool= True
+bool2= False
+bool3= 5 > 3
+bool4= 10 >= 7
+bool5= 1 < 2
+
+
+
+#ოპერატორი არის ნიშანი ან სიმბოლო რომელიც გამოიყენება ცვლადებთან სამუშაოთ (მაგალითად +  - *  /  ==)
+
+#ნასწავლი გვაქვს მარტო ლოგიკური და შედარებითი ოპერაციები 
+
+
+print(8 > 17)
+print(20 < 30)
+print(6 >= 1)
+print(154 <= 125)
+print(99 >= 99)
+
+
+age = (input("your age: "))
+
+print(age <= 18)  
+
