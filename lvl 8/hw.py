370d34a0c0dc232eb3174555e70186d53864d69b
pipias860-droid <pipias860@gmail.com>
2026-03-04 23:17:35 +0400

    commit

--- lvl 8/hw.py
+++ lvl 8/hw.py
@@ -38,7 +38,50 @@ print(154 <= 125)
 print(99 >= 99)
 
 
-age = (input("your age: "))
+age = 15
 
-print(age <= 18)  
+print(age > 10 and age < 18)  
+print(age > 18 or age == 15)  
+print(not(age > 18))          
 
+
+
+
+num1 = float(input("enter 1st number: "))
+num2 = float(input("enter 2nd number: "))
+
+
+if num1 > num2:
+    print("1st number is bigger then the 2nd one")
+else:
+    print("1st number is not bigger then the 2nd one")
+
+
+
+
+
+
+num1 = float(input("eneter 1st number: "))
+num2 = float(input("enter 2nd number: "))
+
+
+if num1 > 10:
+    print("1st number is bigger then 10")
+else:
+    print("1st number is bigger then 10")
+
+
+if num2 > 20:
+    print("2nd number is bi8gger then 20")
+else:
+    print("2nd number is not bigger then 20")
+
+
+
+
+num = int(input("Enter a number: "))
+
+if 100 <= num <= 999:
+    print("The number is between 100 and 999")
+else:
+    print("The number is not between 100 and 999")
