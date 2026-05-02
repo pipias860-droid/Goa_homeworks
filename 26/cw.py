e64889883af9887e3f00dcc07de1e58a1dc3ed47
pipias860-droid <pipias860@gmail.com>
2026-05-02 04:05:57 +0400

    commit

---
+++ 26/cw.py
@@ -0,0 +1,23 @@
+# mylist = ["sandro", 25, True, 2.67, "mr.robot"]
+
+
+# mylist.append("New Element")
+# mylist.insert(3, 8)
+# mylist.pop(1)
+# last_element = mylist.pop()
+# print("ამოღებული ელემენტი:", last_element)
+
+# mylist.reverse()
+
+# print("სია:", mylist)
+
+
+sandrossiabuddie = []
+
+for i in range(5):
+    x = input
+
+sandrossiabuddie.append(x)
+
+
+print(sandrossiabuddie)
---
+++ 26/hw.py
@@ -0,0 +1,64 @@
+# append არის ტეგი რომელიც არსებულ სიას ცვლის (ან და შეიზლება რომ მომხმარებელმა თავისი რამე შემოიტანოს სიაში)
+
+arvici=["sandro", "gio", "nika",]
+
+arvici.append("ana")
+
+print(arvici)
+
+# task 2
+
+meoresia=[]
+
+meoresia.append(input())
+meoresia.append(input())
+meoresia.append(input())
+meoresia.append(input())
+meoresia.append(input())
+
+print(meoresia)
+
+#task 3
+
+mesamesia=["facebook", "instagram", "tiktok",]
+
+mesamesia.remove("tiktok")
+
+print(mesamesia)
+
+# task 4
+
+# remove ით სტრინგი იშლება სახელით ხოლო pop ით რომ წავშალოთ უნდა გამოვიყენოთ ინდექსი
+
+# task 5
+
+mexutedavalebissia=[67,42,52,16]
+
+amoshliliricxvi = mexutedavalebissia.pop(3)
+
+print("amoxebuli ricxvi:", amoshliliricxvi)
+print("sia:", mexutedavalebissia)
+
+# task 6
+
+siaeqvsi=[10,22,45,32,67,42]
+
+siaeqvsi.insert(3, 333)
+
+print(siaeqvsi)
+
+# task 7
+
+idksia = [45, 12, 89, 3, 27]
+idksia.sort()
+print(idksia) 
+
+# task 8 
+
+# reverse უბრალოდ სიას უკუღმა კითხულობს 
+
+magalitisia77= ["sandro","gio","nika"]
+
+magalitisia77.reverse()
+
+print(magalitisia77)
