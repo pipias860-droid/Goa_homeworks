8fbe0efc7dd0824b560a32b49ae51ebcceccd2d9
pipias860-droid <pipias860@gmail.com>
2026-05-16 19:07:56 +0400

    commit

---
+++ lvl30/cw.py
@@ -0,0 +1,20 @@
+
+
+idl = input("your text:")
+
+upercase = 0
+
+lowercase = 0
+
+for i in range (len(idl)):
+   
+      if idl[i]==idl[i].upper():
+            upper=upper+1
+else:
+     lower=lower+1
+     
+
+
+
+
+print("upper" , upper , "lower" , lower)
